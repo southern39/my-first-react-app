a47a4aa369ea6c724b4dfe34f4f19ff52471696f
southern39 <namcaophng.code@gmail.com>
2025-06-11 16:54:45 +0700

    update js bridge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { use, useState, useEffect } from "react";
 
 function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [nativeMessage, setNativeMessage] = useState(null);
 
   const loadData = () => {
     fetch("https://api.github.com/users/octocat")
@@ -11,7 +12,7 @@ function App() {
         sendMessageToNativeApp("Loading data from GitHub API successfully");
         return res.json();
       })
-      
+
       .then((json) => {
         setData(json);
         setError(null);
@@ -28,6 +29,16 @@ function App() {
     }
   }
 
+  function updateFromNative(message) {
+    document.getElementById("inputField").value = message;
+  }
+
+  useEffect(() => {
+    window.receiveMessageFromNative = function (message) {
+      setNativeMessage(message); // Update React state
+    };
+  }, []);
+
   return (
     <div style={{ padding: 20, fontFamily: "sans-serif" }}>
       <h1>Hello from WebView (update 1.1)</h1>
@@ -37,6 +48,8 @@ function App() {
       <pre style={{ marginTop: 20, background: "#f0f0f0", padding: 10 }}>
         {error ? error : data ? JSON.stringify(data, null, 2) : "Click the button to load data from GitHub API."}
       </pre>
+      <h2>Message from native app</h2>
+      <p id="native-message">Message from Native: {nativeMessage ? nativeMessage : "null"}</p>
     </div>
   );
 }
